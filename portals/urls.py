f552e262d54f0d62c726388b866c35465395e897
vmogilev <oracle.ux@gmail.com>
2016-07-19 11:33:13 -0700

    basic clients and index done

--- portals/urls.py
+++ portals/urls.py
@@ -2,6 +2,10 @@ from django.conf.urls import url
 
 from . import views
 
+
+app_name = 'portals'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<pa_code>\w+)/clients/$', views.clients, name='clients'),
 ]
+
--- portals/views.py
+++ portals/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from .models import PortalsAll
 
 
 def index(request):
-    return HttpResponse("Hi Portals here.")
+    portal_list = PortalsAll.objects.all()
+    template = 'portals/index.html'
+    context = {
+        'portal_list': portal_list,
+    }
+    return render(request, template, context)
+
+
+def clients(request, pa_code):
+    portal = get_object_or_404(PortalsAll, pa_code=pa_code)
+    client_list = portal.portalclientsall_set.all()
+    template = 'portals/clients.html'
+    context = {
+        'client_list': client_list,
+    }
+    return render(request, template, context)
