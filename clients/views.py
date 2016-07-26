d58d9f14c937f99c49cd8e25fb84fb7745740f79
vmogilev <oracle.ux@gmail.com>
2016-07-26 16:29:27 -0700

    added client list to categories view and fixed padding BUG on site header

--- clients/views.py
+++ clients/views.py
@@ -6,9 +6,12 @@ def categories(request, pa_code, pca_code):
     portal = get_object_or_404(PortalsAll, pa_code=pa_code)
     client = get_object_or_404(PortalClientsAll, pa_id=portal.pa_id, pca_code=pca_code)
     category_list = client.clientcategoriesall_set.all()
+    client_list = portal.portalclientsall_set.all()
+
     template = 'clients/categories.html'
     context = {
         'category_list': category_list,
+        'client_list': client_list,
         'portal': portal,
         'client': client,
     }
